4114c391e5f5666e522365334ab1a7042d9d5796
bttrthnbfr <hafizjoundys@gmail.com>
2022-06-25 00:09:46 +0700

    update: customize

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,7 @@
+return {
+   [ "tomlion/vim-solidity" ] = {},
+   [ "tpope/vim-fugitive" ] = {},
+   [ "f-person/git-blame.nvim" ] = {},
+   [ 'sbdchd/neoformat' ] = {},
+   [ 'simrat39/symbols-outline.nvim' ] = {},
+}
